11eb36b3b8a21836d8f6b788ace3f58348d85bfc
jyee0012 <jyee12@studentmail.nait.ca>
2018-09-30 11:29:35 -0600

    menu exercise

---
+++ DMIT1513/UITest/UIStuff/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+
+    [SerializeField]
+    Image backgroundImage;
+    [SerializeField]
+    Text musicText, soundText;
+    [SerializeField]
+    Slider musicSlider, soundSlider;
+    [SerializeField]
+    GameObject optionsMenu, pauseMenu, startMenu;
+    // Use this for initialization
+    void Start()
+    {
+        if (optionsMenu != null) optionsMenu.SetActive(false);
+        if (pauseMenu != null) pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region Open/Close Menu
+    public void OpenStartMenu()
+    {
+        startMenu.SetActive(true);
+    }
+    public void CloseStartMenu()
+    {
+        startMenu.SetActive(false);
+    }
+    public void OpenPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+    public void ClosePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+    }
+    public void OpenOptions()
+    {
+        optionsMenu.SetActive(true);
+    }
+    public void CloseOptions()
+    {
+        optionsMenu.SetActive(false);
+    }
+    #endregion
+
+    public void PlayGame()
+    {
+
+    }
+    public void UpdateMusicSoundText()
+    {
+        musicText.text = "Music: " + musicSlider.value + "%";
+        soundText.text = "Sound: " + soundSlider.value + "%";
+    }
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
