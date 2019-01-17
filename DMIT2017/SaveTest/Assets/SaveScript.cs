944bc6a55b0c7aebec81cf5a89152fab73b6019a
jyee0012 <jyee12@studentmail.nait.ca>
2019-01-16 17:07:02 -0700

    found and somewhat fixed problem

--- DMIT2017/SaveTest/Assets/SaveScript.cs
+++ DMIT2017/SaveTest/Assets/SaveScript.cs
@@ -42,7 +42,8 @@ public class SaveContainer
     public static float beginningHeight = 130, buttonSpacing = 35;
 }
 
-public class SaveScript : MonoBehaviour {
+public class SaveScript : MonoBehaviour
+{
 
     SaveContainer allData;
     SaveData myData;
@@ -59,50 +60,119 @@ public class SaveScript : MonoBehaviour {
     [SerializeField]
     Button btnPrefab;
 
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         allData = new SaveContainer();
         myData = new SaveData();
+        profileBtns = new List<Button>();
         LoadData();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     public void CreateLoadProfileBtn()
     {
         Button profilebtn;
         for (int i = 0; i < allData.saveDatas.Count; i++)
         {
-            profilebtn = Instantiate(btnPrefab, btnPrefab.transform.position,btnPrefab.transform.rotation, profileContainer.transform);
+            Vector3 newBtnPos = new Vector3(91.5f, SaveContainer.beginningHeight - (SaveContainer.buttonSpacing * i) - 150, 0);
+            profilebtn = Instantiate(btnPrefab, btnPrefab.transform.position, btnPrefab.transform.rotation, profileContainer.transform);
             profilebtn.GetComponentInChildren<Text>().text = allData.saveDatas[i].GetName();
-            profilebtn.GetComponent<RectTransform>().localPosition = new Vector3(0, SaveContainer.beginningHeight - (SaveContainer.buttonSpacing * i), 0);
+            profilebtn.GetComponent<RectTransform>().localPosition = newBtnPos;
+            //profilebtn.onClick.AddListener(() => { FillLoadedData(i); });
+            switch (i)
+            {
+                case 0:
+                    profilebtn.onClick.AddListener(delegate { FillLoadedData(0); });
+                    break;
+                case 1:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(1));
+                    break;
+                case 2:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(2));
+                    break;
+                case 3:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(3));
+                    break;
+                case 4:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(4));
+                    break;
+                case 5:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(5));
+                    break;
+                case 6:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(6));
+                    break;
+                case 7:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(7));
+                    break;
+                case 8:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(8));
+                    break;
+                case 9:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(9));
+                    break;
+                case 10:
+                    profilebtn.onClick.AddListener(() => FillLoadedData(10));
+                    break;
+            }
+            //Debug.Log(newBtnPos);
             profileBtns.Add(profilebtn);
+        }
+        for (int i = 0; i < profileBtns.Count; i++)
+        {
+            //profileBtns[i].onClick.AddListener(() => FillLoadedData(i));
         }
     }
     public void LoadData()
     {
+        if (profileBtns.Count > 0)
+        {
+            foreach (Transform child in profileContainer.GetComponentsInChildren<Transform>())
+            {
+                if (child.gameObject != profileContainer.gameObject)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+            profileBtns.Clear();
+        }
         if (File.Exists("Data.xml"))
         {
             Stream stream = File.Open("Data.xml", FileMode.Open);
             XmlSerializer serializer = new XmlSerializer(typeof(SaveContainer));
             allData = serializer.Deserialize(stream) as SaveContainer;
             stream.Close();
-            if (allData.saveDatas.Count <= 0)
-            {
-                return;
-            }
-            myData = allData.saveDatas[0];
-            nameField.text = myData.GetName();
-            scoreText.text = "Score: " + myData.GetScore();
-            if (scoreSlider != null)
-            {
-                scoreSlider.value = myData.GetScore();
-            }
         }
         CreateLoadProfileBtn();
 
+    }
+    public void ClickButton(string input = "")
+    {
+        if (input == "") Debug.Log(gameObject.name);
+        else Debug.Log(input);
+
+        FillLoadedData();
+    }
+    public void FillLoadedData(int loadedIndex = 0)
+    {
+        //Debug.Log("I clicked on " + loadedIndex + " button.");
+        if (allData.saveDatas.Count <= 0)
+        {
+            return;
+        }
+        myData = allData.saveDatas[loadedIndex];
+        nameField.text = myData.GetName();
+        scoreText.text = "Score: " + myData.GetScore();
+        if (scoreSlider != null)
+        {
+            scoreSlider.value = myData.GetScore();
+        }
     }
     public void SaveData()
     {
