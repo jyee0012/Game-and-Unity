8e1e9bbe4fbc9fe98eaa9a048c35957652c649be
jyee0012 <jyee12@studentmail.nait.ca>
2019-02-11 16:48:53 -0700

    Changed Camera and playing with other mechanics and implementing puzzles

--- DMIT2017/Multiplayer Puzzle/Assets/Scripts/CameraControl.cs
+++ DMIT2017/Multiplayer Puzzle/Assets/Scripts/CameraControl.cs
@@ -6,42 +6,65 @@ public class CameraControl : MonoBehaviour
 {
 
     [SerializeField]
+    bool cameraMove = true;
+    [SerializeField]
     GameObject player1, player2;
     [SerializeField]
     float playerDist = 0;
-    Vector3 player1Pos, player2Pos, cameraStartPos, newCameraPos;
+    Vector3 player1Pos, player2Pos, cameraStartPos;
     float cameraYPos = 0, cameraXPos = 0, lerpTimer = 0, lerpTargetTime;
     // Use this for initialization
     void Start()
     {
         cameraStartPos = transform.position;
-        newCameraPos = cameraStartPos;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        CameraMovement();
-        newCameraPos.x = cameraXPos;
-        newCameraPos.y = cameraYPos;
-        transform.position = newCameraPos;
-        playerDist = Vector3.Distance(player1Pos, player2Pos);
-        //lerpTimer = lerpTargetTime - Time.time;
-        //if (playerDist > 70)
-        //{
-        //    if (GetComponent<Camera>().fieldOfView == 60) lerpTargetTime = Time.time + 1f;
-        //    GetComponent<Camera>().fieldOfView = Mathf.Lerp(90, 60,1f);
-        //}
+        if (cameraMove)
+        {
+            transform.position = CameraMovement();
+            playerDist = Vector3.Distance(player1Pos, player2Pos);
+            //lerpTimer = lerpTargetTime - Time.time;
+            //if (playerDist > 70)
+            //{
+            //    if (GetComponent<Camera>().fieldOfView == 60) lerpTargetTime = Time.time + 1f;
+            //    GetComponent<Camera>().fieldOfView = Mathf.Lerp(90, 60,1f);
+            //}
+        }
     }
-    void CameraMovement()
+    Vector3 CameraMovement()
     {
-        player1Pos = player1.transform.position;
-        player2Pos = player2.transform.position;
-        // ((big vector - small vector) /2) + small vector = point between 2 vectors
-        //cameraXPos = (player1Pos.x > player2Pos.x) ? (player1Pos.x - player2Pos.x) / 2 + player2Pos.x : (player2Pos.x - player1Pos.x) / 2 + player1Pos.x;
-        //cameraYPos = (player1Pos.y > player2Pos.y) ? (player1Pos.y - player2Pos.y) / 2 + player2Pos.y : (player2Pos.y - player1Pos.y) / 2 + player1Pos.y;
-        cameraXPos = Mathf.Lerp(player1Pos.x, player2Pos.x, 0.5f);
-        cameraYPos = Mathf.Lerp(player1Pos.y, player2Pos.y, 0.5f);
+        Vector3 newCameraPos = cameraStartPos;
+        if (player1 != null)
+        {
+            player1Pos = player1.transform.position;
+            if (player2 != null)
+            {
+                // both players are active
+                player2Pos = player2.transform.position;
+                // ((big vector - small vector) /2) + small vector = point between 2 vectors
+                //cameraXPos = (player1Pos.x > player2Pos.x) ? (player1Pos.x - player2Pos.x) / 2 + player2Pos.x : (player2Pos.x - player1Pos.x) / 2 + player1Pos.x;
+                //cameraYPos = (player1Pos.y > player2Pos.y) ? (player1Pos.y - player2Pos.y) / 2 + player2Pos.y : (player2Pos.y - player1Pos.y) / 2 + player1Pos.y;
+                newCameraPos.x = Mathf.Lerp(player1Pos.x, player2Pos.x, 0.5f);
+                newCameraPos.y = Mathf.Lerp(player1Pos.y, player2Pos.y, 0.5f);
+            }
+            else
+            {
+                // only player 1 is active
+                newCameraPos.x = player1Pos.x;
+                newCameraPos.y = player1Pos.y;
+            }
+        }
+        else
+        {
+            // only player 2 is active
+            player2Pos = player2.transform.position;
+            newCameraPos.x = player2Pos.x;
+            newCameraPos.y = player2Pos.y;
+        }
+        return newCameraPos;
     }
 }
