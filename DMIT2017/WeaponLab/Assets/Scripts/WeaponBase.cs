bd608375bb1ef37f553e1c18e478940f49748fef
jyee0012 <jyee12@studentmail.nait.ca>
2019-02-26 14:55:56 -0700

    Started Weapon Lab

---
+++ DMIT2017/WeaponLab/Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponData
+{
+    public enum WeaponType { Melee, Range }
+    public string name, description;
+    public float damage, attackRate, attackRange;
+    public float currentAmmo, maxAmmo;
+    public float weaponSpread, reloadTime, accuracy, recoil, aimSpd;
+    public WeaponData()
+    {
+
+    }
+    public WeaponData(WeaponData newWeapon)
+    {
+
+    }
+    public void PlayAnimation()
+    {
+
+    }
+    public void PlayParticle()
+    {
+
+    }
+}
+
+public class WeaponBase : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public virtual void Use()
+    {
+
+    }
+    public virtual string GetName()
+    {
+
+    }
+    public virtual float GetCurrAmmo()
+    {
+
+    }
+    public virtual float GetMaxAmmo()
+    {
+
+    }
+}
