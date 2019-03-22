81974fad5711e98ee699730ad1c7470c9d032e94
James <jyee0012@gmail.com>
2019-03-21 22:31:26 -0600

    Progress on lab3

--- DMIT2017/MultiplayerGame/Assets/Scripts/EnemyScript.cs
+++ DMIT2017/MultiplayerGame/Assets/Scripts/EnemyScript.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class EnemyScript : MonoBehaviour
 {
+    private int fingerID = -1;
+    private void Awake()
+    {
+        #if !UNITY_EDITOR
+            fingerID = 0; 
+        #endif
+    }
+    public NavMeshAgent navAgent = null;
+    public Camera cameraController = null;
+    public bool playerControlled = false;
     [SerializeField]
-    NavMeshAgent navAgent = null;
+    bool showMouse = false, showDestination = false;
     [SerializeField]
-    List<PlayerController> playerList = new List<PlayerController>();
+    GameObject mouseIndicator = null;
     [SerializeField]
-    Camera cameraController = null;
-    [SerializeField]
-    bool playerControlled = false;
+    KeyCode setDestinationKey = KeyCode.Mouse0;
 
     PlayerController closestPlayer = null;
 
@@ -20,10 +29,6 @@ public class EnemyScript : MonoBehaviour
     void Start()
     {
         SetupNavAgent();
-        foreach (PlayerController player in FindObjectsOfType<PlayerController>())
-        {
-            playerList.Add(player);
-        }
     }
 
     // Update is called once per frame
@@ -31,9 +36,34 @@ public class EnemyScript : MonoBehaviour
     {
         if (playerControlled)
         {
-            if (cameraController != null)
+            if (Input.GetKeyDown(setDestinationKey))
             {
+                if (cameraController != null)
+                {
+                    RaycastHit clickInfo;
+                    bool hitUI = false;
+                    Ray mouseRay = cameraController.ScreenPointToRay(Input.mousePosition);
+                    if (EventSystem.current.IsPointerOverGameObject(fingerID))    // is the touch on the GUI
+                    {
+                        // GUI Action
+                        hitUI = true;
+                    }
+                    if (Physics.Raycast(mouseRay, out clickInfo) && !hitUI)
+                    {
+                        if (showMouse && mouseIndicator != null)
+                        {
+                            mouseIndicator.transform.position = clickInfo.point;
+                        }
+                        navAgent.SetDestination(clickInfo.point);
+                        if (transform.parent.GetComponent<SpawnerScript>() != null)
+                        {
+                            SpawnerScript spawnParent = transform.parent.GetComponent<SpawnerScript>();
+                            spawnParent.hasDestination = true;
+                            spawnParent.enemyDestination = navAgent.destination;
+                        }
 
+                    }
+                }
             }
         }
         else
@@ -44,7 +74,7 @@ public class EnemyScript : MonoBehaviour
             }
             else
             {
-                FindClosestPlayer();
+                FindClosestPlayer(FindAllPlayers());
             }
         }
     }
@@ -62,12 +92,12 @@ public class EnemyScript : MonoBehaviour
             }
         }
     }
-    void FindClosestPlayer()
+    void FindClosestPlayer(List<PlayerController> playerList)
     {
         if (playerList.Count > 0)
         {
             GameObject newPlayer = playerList[0].gameObject;
-            for(int i = 1; i < playerList.Count; i++)
+            for (int i = 1; i < playerList.Count; i++)
             {
                 float currentDist = Vector3.Distance(transform.position, newPlayer.transform.position),
                     newDist = Vector3.Distance(transform.position, playerList[i].transform.position);
@@ -79,6 +109,38 @@ public class EnemyScript : MonoBehaviour
 
 
             closestPlayer = newPlayer.GetComponent<PlayerController>();
+        }
+    }
+    List<PlayerController> FindAllPlayers()
+    {
+        List<PlayerController> playerList = new List<PlayerController>();
+        foreach (PlayerController player in FindObjectsOfType<PlayerController>())
+        {
+            playerList.Add(player);
+        }
+        return playerList;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Projectile")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        if (collision.transform.tag == "Player")
+        {
+            //collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if (showDestination)
+        {
+            Gizmos.color = Color.yellow;
+            Vector3 aboveDest = navAgent.destination;
+            aboveDest.y += 5f;
+            Gizmos.DrawSphere(aboveDest, 0.5f);
         }
     }
 }
---
+++ DMIT2017/MultiplayerGame/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spawnedPrefab = null;
+    [SerializeField]
+    Vector2 spawnRange = Vector2.one;
+    [SerializeField, Range(0,20)]
+    float spawnDelay = 10;
+    [SerializeField, Range(1,100)]
+    int spawnAmount = 1;
+    [SerializeField]
+    bool canSpawn = true, endlessSpawn = false, drawRange = false;
+
+    [Header("Enemy Info")]
+    [SerializeField]
+    Camera cameraController = null;
+    [SerializeField]
+    bool playerControlled = false;
+    public bool hasDestination = false;
+    public Vector3 enemyDestination = Vector3.zero;
+
+    int spawnCount = 0;
+    float spawnTimeStamp = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimeStamp = spawnDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSpawn && spawnTimeStamp < Time.time)
+        {
+            SpawnPrefab();
+        }
+    }
+    void SpawnPrefab()
+    {
+        if (spawnedPrefab == null) return;
+        if (endlessSpawn || spawnCount < spawnAmount)
+        {
+            Vector3 spawnPos = GetRandomSpawnPoint(spawnRange);
+            GameObject spawned = Instantiate(spawnedPrefab, spawnPos, transform.rotation, transform);
+            if(spawned.GetComponent<EnemyScript>() != null)
+            {
+                EnemyScript spawnedEnemy = spawned.GetComponent<EnemyScript>();
+                spawnedEnemy.playerControlled = playerControlled;
+                spawnedEnemy.cameraController = cameraController;
+                if (hasDestination)
+                {
+                    spawnedEnemy.navAgent.SetDestination(enemyDestination);
+                }
+            }
+            spawnCount++;
+            spawned.name = spawnedPrefab.name + " " + spawnCount;
+            spawnTimeStamp = Time.time + spawnDelay;
+        }
+    }
+    Vector3 GetRandomSpawnPoint(Vector2 range)
+    {
+        Vector3 spawnPoint = transform.position;
+        float randX = Random.Range(0, range.x), randY = Random.Range(0, range.y);
+        spawnPoint.x += randX;
+        spawnPoint.z += randY;
+        return spawnPoint;
+    }
+    private void OnDrawGizmos()
+    {
+        if (drawRange)
+        {
+            Gizmos.color = Color.grey;
+            Gizmos.DrawCube(transform.position, new Vector3(spawnRange.x, 1, spawnRange.y));
+        }
+    }
+}
