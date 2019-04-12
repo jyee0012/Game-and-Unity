cf7d3b00b7b6ff1cb78f407a76752592742c609b
James <jyee0012@gmail.com>
2019-04-11 21:56:42 -0600

    Touched up pong ball physics and added paddle

---
+++ DMIT2017/FinalProject/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 distanceMinMax = Vector2.zero;
+    [SerializeField]
+    Vector3 paddleMoveDirection = Vector3.right;
+    [SerializeField]
+    int playerNum = 0;
+    [SerializeField]
+    bool useController = false, clampMovement = false;
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    Vector3 startPos = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(GetPlayerInput());
+    }
+    Vector3 GetPlayerInput()
+    {
+        #region Controller Controls
+        /*
+        axis1.text = Input.GetAxis("Axis1").ToString(); //left analog horizontal
+        axis2.text = Input.GetAxis("Axis2").ToString(); //left analog vertical
+        axis3.text = Input.GetAxis("Axis3").ToString(); //offset of triggers
+        axis4.text = Input.GetAxis("Axis4").ToString(); //right analog horizontal
+        axis5.text = Input.GetAxis("Axis5").ToString(); //right analog vertical
+        axis6.text = Input.GetAxis("Axis6").ToString(); //d-pad horizontal
+        axis7.text = Input.GetAxis("Axis7").ToString(); //d-pad vertical
+        axis8.text = Input.GetAxis("Axis8").ToString();
+        axis9.text = Input.GetAxis("Axis9").ToString(); //left trigger
+        axis10.text = Input.GetAxis("Axis10").ToString(); //right trigger
+        button0.text = Input.GetAxis("Button0").ToString(); //a button
+        button1.text = Input.GetAxis("Button1").ToString(); //b button
+        button2.text = Input.GetAxis("Button2").ToString(); //x button
+        button3.text = Input.GetAxis("Button3").ToString(); //y button
+        button4.text = Input.GetAxis("Button4").ToString(); //left bumper
+        button5.text = Input.GetAxis("Button5").ToString(); //right bumper
+        button6.text = Input.GetAxis("Button6").ToString(); //back button
+        button7.text = Input.GetAxis("Button7").ToString(); //start button
+        */
+        #endregion
+        float xInput = 0;
+        if (useController)
+        {
+            xInput = Input.GetAxis("Axis1P" + playerNum);
+        }
+        else
+        {
+            xInput = Input.GetAxis("Horizontal");
+        }
+        Vector3 playerDirection = new Vector3(xInput, 0,0);
+        return playerDirection;
+    }
+    void Movement(Vector3 direction)
+    {
+        transform.Translate(direction * Time.deltaTime * moveSpeed);
+        if (clampMovement)
+        {
+            // include checks based on axis
+            // right now this only checks left-right, have it check forward/back and up/down
+            Vector3 clampedPos = transform.position;
+            if (transform.position.x > startPos.x + distanceMinMax.y)
+            {
+                clampedPos.x = startPos.x + distanceMinMax.y;
+            }
+            if (transform.position.x < startPos.x + distanceMinMax.x)
+            {
+                clampedPos.x = startPos.x + distanceMinMax.x;
+            }
+            transform.position = clampedPos;
+        }
+    }
+}
--- DMIT2017/FinalProject/Assets/Scripts/PongBallScript.cs
+++ DMIT2017/FinalProject/Assets/Scripts/PongBallScript.cs
@@ -2,50 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PongBallScript : MonoBehaviour
 {
     public Rigidbody rbody = null;
     [SerializeField]
-    float moveSpeed = 2f, moveForce = 100f, rotateDelay = 0.5f;
+    float moveSpeed = 2f, moveForce = 100f, rotateDelay = 0.5f, moveDelay = 1;
     [SerializeField]
     Vector2 ricochetRange = Vector2.zero;
     [SerializeField]
-    bool moving = false, useForce = false;
+    bool moving = false, useForce = false, canRotate = false, reset = false, additiveForce = false;
 
-    float rotateTimeStamp = 0;
+    Vector3 startPos = Vector3.zero;
+    float rotateTimeStamp = 0, moveTimeStamp = 0;
     // Start is called before the first frame update
     void Start()
     {
+        startPos = transform.position;
         if (rbody == null) rbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moving)
+        if (reset)
         {
-            if (useForce) {
+            ResetPos(reset);
+            reset = !reset;
+        }
+        if (moving && moveTimeStamp < Time.time)
+        {
+            if (useForce)
+            {
                 rbody.AddForce(transform.forward * moveForce);
             }
             else
             {
                 ConstantMovement(transform.forward);
             }
+            moveTimeStamp = Time.time + moveDelay;
         }
     }
-    void ConstantMovement(Vector3 direction) {
+    void ConstantMovement(Vector3 direction)
+    {
         transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (rotateTimeStamp < Time.time) {
+        if (collision.transform.tag == "Paddle")
+        {
+            Debug.Log("Before: " + transform.rotation.eulerAngles);
+            transform.rotation = collision.transform.rotation;
             RandomRotate();
+            Debug.Log("After: " + transform.rotation.eulerAngles);
+            if (additiveForce)
+            {
+                rbody.AddForce(transform.forward * moveForce);
+            }
         }
     }
     void RandomRotate()
     {
-        float randRange = Random.Range(ricochetRange.x, ricochetRange.y);
-        transform.Rotate(new Vector3(0, 180 + randRange, 0));
+        float randRangeX = Random.Range(ricochetRange.x, ricochetRange.y);
+        float randRangeY = Random.Range(ricochetRange.x, ricochetRange.y);
+        float randRangeZ = Random.Range(ricochetRange.x, ricochetRange.y);
+        transform.Rotate(new Vector3(randRangeX, randRangeY, randRangeZ));
         rotateTimeStamp = Time.time + rotateDelay;
+    }
+    void ResetPos(bool reset = false)
+    {
+        if (reset)
+        {
+            transform.position = startPos;
+        }
     }
 }
